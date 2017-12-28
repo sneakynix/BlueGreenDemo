9d4df70152661b718a6808cde8fa25cca4d5d5d6
Dmitry Kuleshov <vdksystem@gmail.com>
2017-12-28 14:53:21 +0200

    change commit from short to full

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ set -x
 echo "Deploying to AWS CodeDeploy"
 
 aws deploy create-deployment \
+  --region us-east-1
   --application-name BGappPoC \
   --deployment-config-name CodeDeployDefault.OneAtATime \
   --deployment-group-name example-group \
   --description "My GitHub BlueGreen deployment demo" \
-  --github-location repository=gallantra/BlueGreenDemo,commitId=$(git rev-parse --short HEAD) \
-  --region us-east-1
+  --github-location repository=gallantra/BlueGreenDemo,commitId=$(git rev-parse HEAD) \
