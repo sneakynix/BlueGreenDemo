edf8022dc0818d694242b9a38843a30efacc4384
Dmitry Kuleshov <vdksystem@gmail.com>
2017-12-28 14:25:06 +0200

    green revision for aws codedeploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Deploying to AWS CodeDeploy"
+
+aws deploy create-deployment \
+  --application-name BGappPoC \
+  --deployment-config-name CodeDeployDefault.OneAtATime \
+  --deployment-group-name example-group \
+  --description "My GitHub BlueGreen deployment demo" \
+  --github-location repository=gallantra/BlueGreenDemo,commitId=${GIT_COMMIT}
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get('/', function (req, res) {
 
-  hello = "Hello World from green version"
+  hello = "Hello World from green version. This is replacement for previous production"
   res.send(hello);
 });
 
