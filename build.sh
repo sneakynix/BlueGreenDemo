3488818aeaf8cf8433c98269e48166c25111a17f
sneakynix <29865719+sneakynix@users.noreply.github.com>
2017-12-29 15:03:44 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,7 @@ echo "Building the application"
 ln -s {$env.WORKSPACE} ~/rpmbuild
 cd ~/rpmbuild
 npm install --save-dev speculate
-
 npm run spec
-
-rpmbuild -bb ~/rpmbuild/SPECS/BlueGreenDemoApp.spec
+/usr/bin/rpmbuild -bb ~/rpmbuild/SPECS/BlueGreenDemoApp.spec
 
 ls ~/rpmbuild/RPMS/x86_64
