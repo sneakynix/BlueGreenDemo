8649d485bf9c1e5c42fbe2e8cd260628b899c319
sneakynix <29865719+sneakynix@users.noreply.github.com>
2017-12-29 15:21:40 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ echo $WORKSPACE
 
 ln -s $WORKSPACE ~/rpmbuild --force
 cd ~/rpmbuild
+npm install --save express
 npm install --save-dev speculate
 npm run spec
 /usr/bin/rpmbuild -bb ~/rpmbuild/SPECS/BlueGreenDemoApp.spec
 
-ls ~/rpmbuild/RPMS/x86_64
+ls -l ~/rpmbuild/RPMS/x86_64
