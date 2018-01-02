024b04228cc70be76d8a2687683b6dacb7d59531
sneakynix <29865719+sneakynix@users.noreply.github.com>
2018-01-02 15:58:16 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,16 @@
 
 echo "[INFO] Building the application"
 
+echo "[INFO] bumping version"
+npm version "1.0.4-$BUILD_NUMBER"
+
 echo "[INFO] Creating RPM package"
 
 ln -s $WORKSPACE ~/rpmbuild --force
 cd ~/rpmbuild
 npm install --save express
 npm install --save-dev speculate
+
 npm run spec
 /usr/bin/rpmbuild -bb ~/rpmbuild/SPECS/BlueGreenDemoApp.spec
 ls -l ~/rpmbuild/RPMS/x86_64
